0072d30f1a2affc4aec610244e95f63906123e28
BRUNO GONÇALVES <142263669+brunosg-on@users.noreply.github.com>
2025-03-12 16:14:40 +0000

    ConversaoDeNotasParaConceito

--- Aula 2/ConversaoDeNotasParaConceito.java
+++ Aula 2/ConversaoDeNotasParaConceito.java
@@ -1,3 +1,34 @@
+import java.util.Scanner;
 public class ConversaoDeNotasParaConceito {
-    
+    public static void main(String[] args)
+    {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite uma nota: ");
+        double nota = entrada.nextDouble();
+        if(nota < 3)
+        {
+            System.out.println("E");
+        }
+        else
+        {
+            if(nota < 5)
+            {
+                System.out.println("D");
+            }
+            if(nota < 7)
+            {
+                System.out.println("C");
+            }
+            if(nota < 9)
+            {
+                System.out.println("B");
+            }
+            else
+            {
+                System.out.println("A");
+            }
+        }
+        entrada.close();
+    }
+     
 }
