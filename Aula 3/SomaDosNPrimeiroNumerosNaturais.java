82469ff712a574c59a487c0c726622258a7e9387
BRUNO GONÇALVES <142263669+brunosg-on@users.noreply.github.com>
2025-03-12 16:35:45 +0000

    SomaDosNPrimeiroNumerosNaturais

---
+++ Aula 3/SomaDosNPrimeiroNumerosNaturais.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class SomaDosNPrimeiroNumerosNaturais
+{
+    public static void main (String[] args)
+    {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite um numero: ");
+        int numero = entrada.nextInt(), i, soma = 0;
+
+        for(i=1;i <= numero;i++)
+        {
+            soma += i;
+        }
+        System.out.println (soma);
+        entrada.close();
+    }
+
+}
