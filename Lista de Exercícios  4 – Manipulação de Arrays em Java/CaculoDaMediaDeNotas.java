194597b18160186d75d96a2a035dd9b6cb3154b9
BRUNO GONÇALVES <142263669+brunosg-on@users.noreply.github.com>
2025-03-14 18:58:20 +0000

    Descrição: Desenvolva um programa que peça ao usuário para inserir 4 notas e armazene-as em um array. Depois, o programa deve calcular e exibir a média das notas. Requisitos: • Criar um array de tamanho 4 para armazenar as notas. • Utilizar um loop for para somar todas as notas e calcular a média. • Se a média for maior ou igual a 7, exibir "Aprovado", senão, exibir "Reprovado".

---
+++ Lista de Exercícios  4 – Manipulação de Arrays em Java/CaculoDaMediaDeNotas.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class CaculoDaMediaDeNotas 
+{
+    public static void main(String[] args)
+    {
+        Scanner entrada = new Scanner(System.in);
+        int nota[] = new int[4], i;
+        double media = 0;
+       
+
+        for(i=0;i<4; i++)
+        {
+            System.out.println("Digite a nota: "+ (i+1)+ "/" + 4);
+            nota[i] = entrada.nextInt();
+            media += nota[i];
+        }
+        media /= 4;
+        if ((media) < 7) 
+        {
+            System.out.println("media = "+ media + " Reprovado");
+        }
+       else
+       {
+        System.out.println("media = "+ media + " Aprovado");
+       }
+        entrada.close();
+
+    }
+}
