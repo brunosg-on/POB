35e833617d77a6c86a6828589de89fb862ad98c0
BRUNO SANTOS <brunosg-on@outlook.com>
2025-03-16 11:47:35 -0300

    Crie um programa que solicite ao usuário 8 números inteiros e armazene-os em um array. O programa deve contar e exibir a quantidade de números pares no array. Requisitos: • Criar um array de tamanho 8. • Utilizar um loop for para percorrer o array e contar quantos números são pares. • Exibir a quantidade de números pares encontrados.

---
+++ Lista de Exercícios  4 – Manipulação de Arrays em Java/ContagemDeNumerosParesEmUmArray.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class ContagemDeNumerosParesEmUmArray 
+{
+ public static void main(String[] args) 
+ {
+    Scanner entrada = new Scanner(System.in);
+    
+    entrada.close();   
+ }   
+}
