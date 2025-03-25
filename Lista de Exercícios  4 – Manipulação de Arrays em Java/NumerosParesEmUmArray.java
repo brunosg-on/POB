3939603195dbe7c00880c725e46acb074d99b878
BRUNO GONÇALVES <brunosg-on@outlook.com>
2025-03-25 12:47:27 -0300

    Criar um array de tamanho 8. • Utilizar um loop for para percorrer o array e contar quantos números são pares. • Exibir a quantidade de números pares encontrados.

--- Lista de Exercícios  4 – Manipulação de Arrays em Java/NumerosParesEmUmArray.java
+++ Lista de Exercícios  4 – Manipulação de Arrays em Java/NumerosParesEmUmArray.java
@@ -1,7 +1,32 @@
+
+import java.util.Scanner;
+
 public class NumerosParesEmUmArray 
 {
     public static void main(String[] args) 
     {
-         
+        Scanner sc = new Scanner(System.in);
+        int vet[] = new int[8];
+        int cont = 0;
+        
+        for(int i=0;i<8;i++)
+        {
+            System.out.println("Digite um numero");
+            vet[i] = sc.nextInt();
+            if(vet[i]%2 == 0)
+            {
+                cont++;
+            }
+            
+        }
+
+        for(int i=0;i<8;i++)
+        {
+            if(vet[i]/2 == 0)
+            {
+                System.out.println(vet[i]);
+            } 
+        }
+        System.out.println("numeros pares "+cont);   
     }
 }
