e655e4c75a911d707747b44b4487f34893284068
BRUNO GONÇALVES <142263669+brunosg-on@users.noreply.github.com>
2025-04-14 17:24:29 -0300

    Domino Saldo

---
+++ Lista de Exercícios 6  – Criação de Classes e Pacotes em Java/DominioContaBancaria.java
@@ -0,0 +1,51 @@
+public class DominioContaBancaria 
+{
+    private String titular;
+    private double saldo;
+
+    public DominioContaBancaria(String titular, double saldo)
+    {
+        this.titular = titular;
+        this.saldo = 0.0;
+    }
+    public String getTitular()
+    {
+        return titular;
+    }
+    public void setTitular(String titular)
+    {
+        this.titular = titular;
+    }
+    public double getSaldo()
+    {
+        return saldo;
+    }
+    public void setSaldo(double saldo)
+    {
+        this.saldo = saldo;
+    }
+    public void depositar(double valor)
+    {
+        if(valor > 0)
+        {
+            saldo += valor;
+            System.out.println("Deposito de R$ "+ valor + "realizado com sucesso!");
+        }
+        else
+        System.out.println("Saldo insuficiente!");
+    }
+    public void sacar(double valor)
+    {
+        if(valor <= saldo)
+        {
+            saldo -= valor;
+            System.out.println("Saque de R$ "+ valor + "realizado com sucesso!");
+        }
+         else
+        System.out.println("Saldo insuficiente!");
+    }
+    public void exibirSaldo()
+    {
+        System.out.println("Saldo Todal de %s: %.2f", titular, saldo);
+    }
+}
