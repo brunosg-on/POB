c22766e623fb1f8e50c7e179079ba3678b41a99b
BRUNO GONÇALVES <brunosg-on@outlook.com>
2025-04-19 11:29:57 -0300

    update

---
+++ Lista de Exercícios  5 – Manipulação de ArrayList em Java/Exercício 3 Soma de Números e Cálculo da Média/Media.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Media 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Double> notaA = new ArrayList<>();
+        System.out.println("Digite uma nota:");
+        double nota = sc.nextDouble();
+        while(nota!=-1)
+        {
+            
+        }
+        sc.close();
+    }
+}
