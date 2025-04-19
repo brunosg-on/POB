d148ba42b378a0fcdcfeb22bed96dbb6a93e45cd
BRUNO GONÇALVES <brunosg-on@outlook.com>
2025-04-19 13:33:59 -0300

    Exercício 4: Validação de Nota de um Aluno Descrição: Crie um programa que leia uma nota de um aluno e verifique sua validade. Se for válida, classifique o aluno conforme sua média. Requisitos: 1. Criar o pacote dominio contendo a classe Aluno:
    o Atributos: nome (String), nota (double).
    o Métodos:
    ▪ validarNota(): verifica se a nota está entre 0 e 10.
    ▪ classificarAluno(): classifica o aluno:
    ▪ Nota >= 7 → "Aprovado"
    ▪ Nota >= 5 e < 7 → "Recuperação"
    ▪ Nota < 5 → "Reprovado"
    
    2. Criar o pacote controle contendo a classe GerenciadorAluno:
    o Solicitar o nome e a nota do aluno e verificar sua validade antes de
    exibir a classificação.
    o Utilizar um while para garantir que o usuário informe uma nota
    válida.

--- Lista de Exercícios  5 – Manipulação de ArrayList em Java/Exercício 2 Remover um Elemento da Lista/Remove.java
+++ Lista de Exercícios  5 – Manipulação de ArrayList em Java/Exercício 2 Remover um Elemento da Lista/Remove.java
@@ -5,23 +5,16 @@ public class Remove
     public static void main(String[] args) 
     {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> Anome = new ArrayList<String>();
+        ArrayList<String> Anome = new ArrayList<>();
 
         for(int i=0;i<5;i++)
         {
             System.out.println("Digite um nome para adicinar:");
             Anome.add(sc.nextLine());
         }
-        System.out.println("Digite um nome para remover:");
-        String nomeR = sc.nextLine();
 
-        for(int i=0;i<5;i++)
-        {
-            if(Anome.remove(nomeR))
-            {
-                System.out.println("Nome Removido:");
-            }
-        }
+        System.out.println("Digite um nome para remover:");
+        Anome.remove(sc.nextLine());
         System.out.println(Anome);
         sc.close();
     }
--- Lista de Exercícios  5 – Manipulação de ArrayList em Java/Exercício 4 Contagem de Ocorrências de um Número/Ocorrencias.java
+++ Lista de Exercícios  5 – Manipulação de ArrayList em Java/Exercício 4 Contagem de Ocorrências de um Número/Ocorrencias.java
@@ -1,10 +1,28 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Ocorrencias 
 {
   public static void main(String[] args) 
   {
     Scanner scanner = new Scanner(System.in);
-    
+    int cont = 0;
+    ArrayList<Integer> numeros = new ArrayList<>(); 
+    for(int i=0;i<10;i++)
+    {
+      System.out.printf("Digite um numero %d/%d\n", i+1, 10);
+      numeros.add(scanner.nextInt());
+    }
+    System.out.println("Digite um numero para contagem de ocorrencia");
+    int numero = scanner.nextInt();
+    for(int i:numeros)
+    {
+      if (i == numero) 
+      {
+          cont++;
+      }
+    }
+
+    System.out.println(cont);
     scanner.close(); 
   }  
 }
